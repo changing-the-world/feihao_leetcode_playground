9be0d0289f37f7b7795815c3ab74ed456cf0d927
GreatFeihao <feihao8703@163.com>
2019-04-08 22:57:53 +0800

    Create leetcode132.py

---
+++ leetcode132.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def minCut(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) < 2:
+            return 0
+        same_len = 1
+        
+        div_cnt = [0]
+        last_palindrome_list = []
+        for idx in xrange(1, len(s)):
+            cur_palindrome_list = []
+            if s[idx] == s[idx - 1]:
+                cur_same_len = same_len + 1
+            else:
+                cur_same_len = 1
+                cur_palindrome_list.append(1)
+                if same_len > 1 and idx - 1 - same_len >=0 and s[idx -1 - same_len] == s[idx]:
+                    cur_palindrome_list.append(same_len + 2)
+                                
+            for p in last_palindrome_list:
+                if idx - 1 - p >= 0:
+                    if s[idx - 1 - p] == s[idx]:
+                        cur_palindrome_list.append(p + 2)
+            
+            div_cur = div_cnt[idx - 1]
+            if cur_same_len == idx + 1:
+                div_cur = -1
+        
+            for sl in xrange(2, cur_same_len + 1):
+                if div_cnt[idx - sl] < div_cur:
+                    div_cur = div_cnt[idx - sl]
+            for p in cur_palindrome_list:
+                if p == idx + 1:
+                    div_cur = -1
+                if div_cnt[idx - p] < div_cur:
+                    div_cur = div_cnt[idx - p]
+            div_cur += 1
+            div_cnt.append(div_cur)
+            # print div_cnt, same_len, cur_same_len, last_palindrome_list, cur_palindrome_list
+            same_len = cur_same_len
+            last_palindrome_list = cur_palindrome_list
+        return div_cnt[-1]
